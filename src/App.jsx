cd7a23cf15e7b30e0c95844ce0d7d06e6fcfcd3f
lmndang-dev <lmndang@outlook.com>
2025-05-14 02:36:45 -0400

    added picture to monster using API with ID in monster array

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import './App.css'
 //Card will pass down the value of props
 //The props will be pass down menu from an array and destructured the array by value name and email
 //Destructured the array then already know the value will be passed down, in this case is name and email
-const Card = ({name, email}) => {
+const Card = ({name, email, id}) => {
   return (
   <div>
+    <img src={`https://robohash.org/${id}?set=set2`} alt="" />
     <h3>{name}</h3>
     <div>{email}</div>
   </div>
@@ -34,10 +35,10 @@ const App = () => {
         //props: email "Sincere@april.biz" name "Leanne Graham"
         //key is the unique identify for each card - key is required
         //idx is the number of index in each item on the array, start with 0
-        //{name. email} is destructured the object form the arrays
-        monsters.map(({name, email}, idx) => {
+        //{name. email, id} is destructured the object form the arrays
+        monsters.map(({name, email, id}, idx) => {
           console.log(idx);
-          return <Card key={`${name}-${idx}`} name={name} email={email}></Card>
+          return <Card key={`${name}-${idx}`} name={name} email={email} id={id}></Card>
         })
       }
     </>
