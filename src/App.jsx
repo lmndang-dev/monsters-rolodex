93bd4236c1d105d7018906d09edc2e91abe6ccc0
lmndang-dev <lmndang@outlook.com>
2025-05-14 11:08:23 -0400

    Add filtered monsters function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list.component";
-// import Card from "./components/card/card.component";
 import "./App.css";
 
 const App = () => {
   //React hook set initial value to monsters is empty array
   const [monsters, setMonsters] = useState([]);
+
+  //React hook set initial value to filteredMonsters is empty array
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
 
   //Empty array[] meaning only load function onces when the page mounted
   useEffect(() => {
@@ -14,12 +16,33 @@ const App = () => {
     //Set the monsters to that JSON data
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((data) => setMonsters(data));
+      .then((data) => {
+        //Set initial value to monsters and filteredMonsters
+        //Make the first loading to show all the monsters
+        setMonsters(data);
+        setFilteredMonsters(data);
+      });
   }, []);
 
   return (
     <>
-      <CardList monsters={monsters} />
+      <input
+        type="search"
+        onChange={(event) => {
+          //Get the value from the input field and convert it to lower case
+          const searchString = event.target.value.toLowerCase();
+
+          //Create a new array filteredMonsters and find the monsters that match the searchString
+          const newFilteredMonsters = monsters.filter((monsters) =>
+            monsters.name.toLowerCase().includes(searchString)
+          );
+
+          //Set the filteredMonsters to the new array
+          //This will update the filteredMonsters to show only the monsters that match the searchString
+          setFilteredMonsters(newFilteredMonsters);
+        }}
+      />
+      <CardList monsters={filteredMonsters} />
     </>
   );
 };
